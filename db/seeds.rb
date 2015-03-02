d2718277e87431a0c0f9d98c2772c3e18842dde8
Mary <maryskr93@gmail.com>
2015-03-02 16:24:45 +0500

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,21 @@
 Article.destroy_all
 
 Article.create(
-  title: 'The Beatles',
-  content: 'The Beatles were an English rock band that formed in Liverpool in 1960. 
-            With members John Lennon, Paul McCartney, George Harrison and Ringo Starr, they became widely regarded as the greatest and most influential act of the rock era.'
+  title: 'PENGUINS',
+  content: 'Penguins are aquatic, flightless birds that are highly adapted to life in the water. 
+            Their distinct tuxedo-like appearance is called countershading, a form of camouflage that helps keep them safe in the water. 
+            Penguins do have wing-bones, though they are flipper-like and extremely suited to swimming. 
+            Penguins are found almost exclusively in the southern hemisphere, where they catch their food underwater and raise their young on land.'
 )
 
 Comment.destroy_all
 
 Comment.create([
-  { user_name: 'John Lennon', user_email: 'Jonh9@mail.com', content: 'All you need is love', article_id: Article.first.id },
-  { user_name: 'Paul McCartney', user_email: 'Linda@mail.com', content: 'Love is all you need', article_id: Article.first.id }
+  { user_name: 'Lolo', user_email: 'Jonh9@mail.com', content: 'Penguins can be found on every continent in the Southern Hemisphere from the tropical Galapagos Islands (the Galapagos penguin) located near South America to Antarctica (the emperor penguin).', article_id: Article.first.id },
+  { user_name: 'Pepe', user_email: 'Linda@mail.com', content: 'Penguins can spend up to 75% of their lives in the water. They do all of their hunting in the water. Their prey can be found within 60 feet of the surface, so penguins have no need to swim in deep water. They catch prey in their beaks and swallow them whole as they swim. Some species only leave the water for molting and breeding.', article_id: Article.first.id }
   ])
+
+Subcomment.destroy_all
+
+Subcomment.create(comment_id: Comment.first.id, child_id: Comment.last.id, deeps: 1)
+Comment.last.update_attributes(parent_id: Comment.first.id)
